2859bd5e435610aff37c141a8ee476fa88399abb
Christopher Besch <christopher.besch@gmx.de>
2021-07-21 15:01:25 +0200

    started version comparison

--- host_system_scripts/check_requirements.py
+++ host_system_scripts/check_requirements.py
@@ -1,6 +1,7 @@
 import csv
 import subprocess
 import re
+import sys
 from typing import Tuple
 
 
@@ -17,28 +18,54 @@ class Requirement:
 
 
 # check if min version is satisfied with output of version check command
-def check_version(req: Requirement, output: str) -> Tuple[bool, str]:
+def satisfied(req: Requirement, installed_version) -> bool:
+    min_parts = req.min_version.split(".")
+    ins_parts = installed_version.split(".")
+
+    # ignore any part that only exists in one
+    for part in zip(min_parts, ins_parts):
+        # todo: letters missing
+        # required part is bigger than installed
+        if part[0] > part[1]:
+            return False
+        if part[0] < part[1]:
+            return True
+    # everything is the same
+    return True
+
+
+# get installed version from command output
+def get_installed_version(req: Requirement, output: str) -> str:
     pattern = re.compile(req.version_regex_pattern)
     match = pattern.match(output)
     if match is not None:
-        return False, match.group()
+        return match.group()
     raise ValueError(f"regex broken for {req}")
 
 
-def check_pkg(req: Requirement) -> None:
+# return False if not satisfied
+def check_pkg(req: Requirement) -> bool:
+    print(f"checking {req.name}: ...\r", end="")
     try:
         output = subprocess.check_output(
             req.command, stderr=subprocess.STDOUT).decode()
-        satisfied, installed_version = check_version(req, output)
-        if not satisfied:
+
+        installed_version = get_installed_version(req, output)
+        print(installed_version)
+
+        if not satisfied(req, installed_version):
             print(
-                f"'{req.name}' requires version '{req.min_version}' but only version '{installed_version}' is installed!")
-
+                f"checking {req.name}: required version is '{req.min_version}' but only version '{installed_version}' is installed!")
+            return False
+        else:
+            print(f"checking {req.name}: ok")
+            return True
     except FileNotFoundError:
-        print(f"'{req.name}' is not installed!")
+        print(f"checking {req.name}: package not installed!")
+        return False
 
 
-def main():
+def main() -> int:
     # load csv
     with open("requirements.csv", "r", newline="") as file:
         raw_requirements = csv.DictReader(file, delimiter=";")
@@ -46,9 +73,12 @@ def main():
                                     req["min_version"],
                                     req["command"],
                                     req["version_regex_pattern"]) for req in raw_requirements]
+    all_ok = True
     for req in requirements:
-        check_pkg(req)
+        if not check_pkg(req):
+            all_ok = False
+    return 0 if all_ok else 1
 
 
 if __name__ == "__main__":
-    main()
+    sys.exit(main())
