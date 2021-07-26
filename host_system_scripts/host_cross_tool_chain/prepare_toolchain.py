fb5b7b7611cedf466b3eb794026c640f76b42c4d
Christopher Besch <christopher.besch@gmx.de>
2021-07-26 16:45:39 +0200

    create minimal file layout

---
+++ host_system_scripts/host_cross_tool_chain/prepare_toolchain.py
@@ -0,0 +1,17 @@
+import sys
+import os
+
+
+def main():
+    if len(sys.argv) < 2:
+        raise ValueError("Add path to LFS mount point as first argument")
+    lfs_dir = sys.argv[1]
+
+    os.chdir(lfs_dir)
+    folders = ["bin", "etc", "lib", "lib64", "sbin", "usr", "var"]
+    for folder in folders:
+        os.mkdir(folder)
+
+
+if __name__ == "__main__":
+    main()
