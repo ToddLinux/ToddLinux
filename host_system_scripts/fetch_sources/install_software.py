493465436f31dcf6672d59580e326a14acc39933
Christopher Besch <christopher.besch@gmx.de>
2021-07-26 15:35:05 +0200

    list cwd

--- host_system_scripts/fetch_sources/install_software.py
+++ host_system_scripts/fetch_sources/install_software.py
@@ -1,5 +1,16 @@
+import sys
+import os
+
+
 def main():
-    pass
+    if len(sys.argv) < 2:
+        raise ValueError("Add path to LFS mount point as first argument")
+    lfs_dir = sys.argv[1]
+    os.chdir(lfs_dir)
+    os.mkdir("src")
+    print(os.getcwd())
+    print(os.getcwd())
+    print(os.listdir(os.getcwd()))
 
 
 if __name__ == "__main__":
