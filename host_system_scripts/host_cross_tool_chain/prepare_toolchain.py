093754b033529f73fb18c2445187b4fcda31519d
Christopher Besch <christopher.besch@gmx.de>
2021-07-28 16:12:21 +0200

    started build

--- host_system_scripts/host_cross_tool_chain/prepare_toolchain.py
+++ host_system_scripts/host_cross_tool_chain/prepare_toolchain.py
@@ -1,6 +1,10 @@
 # See LICENSE for license details.
 import sys
 import os
+import yaml
+import pathlib
+
+file_dir_path = pathlib.Path(__file__).parent.resolve()
 
 
 def main():
@@ -8,10 +12,17 @@ def main():
         raise ValueError("Add path to LFS mount point as first argument")
     lfs_dir = sys.argv[1]
 
+    print("creating minimal directory layout")
     os.chdir(lfs_dir)
-    folders = ["bin", "etc", "lib", "lib64", "sbin", "usr", "var"]
+    folders = ["bin", "etc", "lib", "lib64", "sbin", "usr", "var", "tools"]
     for folder in folders:
         os.mkdir(folder)
+
+    # start build
+    with open(f"{file_dir_path}/build.yaml", "r") as file:
+        build = yaml.safe_load(file)
+
+    print(build)
 
 
 if __name__ == "__main__":
