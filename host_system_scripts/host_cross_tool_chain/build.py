137bab2840e163a71df6e11413e752753c9685a2
Christopher Besch <christopher.besch@gmx.de>
2021-08-03 15:54:11 +0200

    updated build.py

--- host_system_scripts/host_cross_tool_chain/build.py
+++ host_system_scripts/host_cross_tool_chain/build.py
@@ -38,22 +38,29 @@ def main():
 
     for build in builds:
         try:
-            print(f"building {build.target}: creating build folder...\r")
+            print(f"building {build.target}: creating target folder...\r")
             os.mkdir(f"builds/{build.target}")
 
             for source in build.src_packages:
                 print(
-                    f"building {build.target}: copying source '{source}'...\r")
+                    f"building {build.target}: copying source '{source}'...                \r")
                 # find archive
                 dirs = os.listdir(f"src/{source}")
                 if len(dirs) != 1:
                     print(
-                        f"building {build.target}: copying failed; not only one archive found")
+                        f"building {build.target}: copying failed; not only one archive found             ")
                     raise ValueError
                 shutil.copy(
                     f"src/{source}/{dirs[0]}", f"builds/{build.target}")
         except ValueError:
             continue
+
+        print(
+            f"building {build.target}: execute build script...                      \r")
+        if os.system(f"{file_dir_path}/build_scripts/{build.target}.sh") != 0:
+            print(
+                f"building {build.target}: build script failed...                 \r")
+        print(f"building {build.target}: ok                      ")
 
 
 if __name__ == "__main__":
