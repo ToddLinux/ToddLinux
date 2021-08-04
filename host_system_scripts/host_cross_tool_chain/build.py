5dae6eead86bb063f96853bf2b9a44f2d5b0a73b
Christopher Besch <christopher.besch@gmx.de>
2021-08-04 16:05:47 +0200

    updated build.py + added github actions

--- host_system_scripts/host_cross_tool_chain/build.py
+++ host_system_scripts/host_cross_tool_chain/build.py
@@ -4,18 +4,19 @@ import os
 import shutil
 import csv
 import pathlib
+import subprocess
 
 file_dir_path = pathlib.Path(__file__).parent.resolve()
 
 
 class Build:
-    def __init__(self, target: str, src_packages: str, build_script: str):
-        self.target = target
+    def __init__(self, package: str, src_packages: str, build_script: str):
+        self.package = package
         self.src_packages = src_packages.split(":")
         self.build_script = build_script
 
     def __repr__(self):
-        return f"<Build target: '{self.target}' src_packages: {self.src_packages} build_script: '{self.build_script}'>"
+        return f"<Build package: '{self.package}' src_packages: {self.src_packages} build_script: '{self.build_script}'>"
 
 
 def build_targets(lfs_dir: str, quiet_mode: bool):
@@ -27,48 +28,69 @@ def build_targets(lfs_dir: str, quiet_mode: bool):
                "sbin", "usr", "var", "tools", "builds"]
     # folders = ["builds", "tools"]
     for folder in folders:
-        os.mkdir(folder)
+        if not os.path.isdir(folder):
+            os.mkdir(folder)
 
     # start build
     with open(f"{file_dir_path}/builds.csv", "r", newline="") as file:
         raw_builds = csv.DictReader(file, delimiter=";")
-        builds = [Build(build["target"], build["src_packages"],
+        builds = [Build(build["package"], build["src_packages"],
                         build["build_script"]) for build in raw_builds]
 
-    for build in builds:
-        try:
-            print(f"building {build.target}: creating target folder...\r")
-            os.mkdir(f"builds/{build.target}")
+    finished_builds = []
+    if os.path.isfile("builds/builds.lock"):
+        with open("builds/builds.lock", "r", newline="") as file:
+            # read already completed builds
+            finished_builds = [line.strip() for line in file.readlines()]
 
-            for source in build.src_packages:
+    with open("builds/builds.lock", "a", newline="") as file:
+        for build in builds:
+            if build.package in finished_builds:
+                print(f"building {build.package}: already built")
+                continue
+            try:
                 print(
-                    f"building {build.target}: copying source '{source}'...                \r")
-                # find archive
-                dirs = os.listdir(f"src/{source}")
-                if len(dirs) != 1:
-                    print(
-                        f"building {build.target}: copying failed; not only one archive found             ")
-                    raise ValueError
-                shutil.copy(
-                    f"src/{source}/{dirs[0]}", f"builds/{build.target}")
-        except ValueError:
-            continue
+                    f"building {build.package}: creating package folder...\r", end="")
+                if os.path.isdir(f"builds/{build.package}"):
+                    shutil.rmtree(f"builds/{build.package}")
+                os.mkdir(f"builds/{build.package}")
 
-        print(
-            f"building {build.target}: execute build script...                      \r")
-        os.chdir(f"builds/{build.target}")
-        if os.system(f"{file_dir_path}/build_scripts/{build.build_script}" + output_redirect) != 0:
+                # copy all sources
+                for source in build.src_packages:
+                    print(
+                        f"building {build.package}: copying source '{source}'...                \r", end="")
+                    # find archive
+                    dirs = os.listdir(f"src/{source}")
+                    if len(dirs) != 1:
+                        print(
+                            f"building {build.package}: copying failed; not only one archive found             ", end="")
+                        raise ValueError
+                    shutil.copy(
+                        f"src/{source}/{dirs[0]}", f"builds/{build.package}")
+            except ValueError:
+                continue
+
             print(
-                f"building {build.target}: build script failed...                 ")
-            continue
-        print(f"building {build.target}: ok                      ")
-        os.chdir(lfs_dir)
+                f"building {build.package}: execute build script...                      \r", end="")
+            os.chdir(f"builds/{build.package}")
+            if os.system(f"{file_dir_path}/build_scripts/{build.build_script}" + output_redirect) != 0:
+                print(
+                    f"building {build.package}: build script failed...                 ", end="")
+                continue
+            print(f"building {build.package}: ok                      ")
+            os.chdir(lfs_dir)
+
+            # package built
+            file.write(f"{build.package}\n")
+            file.flush()
 
 
 def set_environ_variables(lfs_dir):
     os.environ["LFS"] = lfs_dir
     os.environ["LFS_TGT"] = "x86_64-lfs-linux-gnu"
-    os.environ["MAKEFLAGS"] ='-j4'
+    output = subprocess.check_output(
+        "nproc", stderr=subprocess.STDOUT).decode()
+    os.environ["MAKEFLAGS"] = f"-j{output}"
     os.environ["PATH"] = lfs_dir + "/tools/bin:" + os.environ["PATH"]
 
 
@@ -82,6 +104,7 @@ def main():
 
     set_environ_variables(lfs_dir)
     build_targets(lfs_dir, quiet_mode)
+
 
 if __name__ == "__main__":
     main()
