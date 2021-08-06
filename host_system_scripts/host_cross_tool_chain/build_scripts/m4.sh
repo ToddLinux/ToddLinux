7d12efeb81d52886c49224b5af4dbf0c44bb746b
Christopher Besch <christopher.besch@gmx.de>
2021-08-06 15:12:45 +0200

    added m4 build script

---
+++ host_system_scripts/host_cross_tool_chain/build_scripts/m4.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+set -euo pipefail
+
+unpack_src() {
+    tar xf m4-1.4.18.tar.xz
+    cd m4-1.4.18.tar.xz
+    return
+}
+
+configure() {
+    sed -i 's/IO_ftrylockfile/IO_EOF_SEEN/' lib/*.c
+    echo "#define _IO_IN_BACKUP 0x100" >> lib/stdio-impl.h
+
+    ./configure --prefix=/usr   \
+                --host=$LFS_TGT \
+                --build=$(build-aux/config.guess)
+    return
+}
+
+make_install() {
+    make && make DESTDIR=$LFS install
+    return
+}
+
+unpack_src && configure && make_install
