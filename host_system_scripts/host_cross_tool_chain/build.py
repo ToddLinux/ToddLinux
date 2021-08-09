cd012d8a09ff6c301141172e2ec1e206f8267415
Adam Jędrzejewski <adamjedrzejewski@icloud.com>
2021-08-09 17:25:11 +0200

    decreased number of concurrent jobs

--- host_system_scripts/host_cross_tool_chain/build.py
+++ host_system_scripts/host_cross_tool_chain/build.py
@@ -89,7 +89,7 @@ def set_environ_variables(lfs_dir):
     os.environ["LFS_TGT"] = "x86_64-lfs-linux-gnu"
     output = subprocess.check_output(
         "nproc", stderr=subprocess.STDOUT).decode()
-    os.environ["MAKEFLAGS"] = f"-j{output}"
+    os.environ["MAKEFLAGS"] = f"-j{int(output) - 1}"
     os.environ["PATH"] = lfs_dir + "/tools/bin:" + os.environ["PATH"]
 
 
