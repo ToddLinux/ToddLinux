916eaf8eccd6cec0437252c0fd37f63704c00fba
Adam Jędrzejewski <adamjedrzejewski@icloud.com>
2021-08-14 21:47:27 +0200

    gcc second pass build script

---
+++ host_system_scripts/host_cross_tool_chain/build_scripts/gcc_pass2.sh
@@ -0,0 +1,43 @@
+# See LICENSE for license details.
+#!/bin/bash
+set -euo pipefail
+
+unpack_src() {
+    tar xf gcc-10.2.0.tar.xz && cd gcc-10.2.0 && \
+        tar -xf ../mpfr-4.1.0.tar.xz && mv mpfr-4.1.0 mpfr && \
+        tar -xf ../gmp-6.2.1.tar.xz && mv gmp-6.2.1 gmp && \
+        tar -xf ../mpc-1.2.1.tar.gz && mv mpc-1.2.1 mpc && \
+        sed -e '/m64=/s/lib64/lib/' -i.orig gcc/config/i386/t-linux64
+    return
+}
+
+configure() {
+    mkdir build && cd build && \
+    mkdir -pv $LFS_TGT/libgcc && \
+    ln -s ../../../libgcc/gthr-posix.h $LFS_TGT/libgcc/gthr-default.h && \
+    ../configure \
+        --build=$(../config.guess) \
+        --host=$LFS_TGT \
+        --prefix=/usr \
+        CC_FOR_TARGET=$LFS_TGT-gcc \
+        --with-build-sysroot=$LFS \
+        --enable-initfini-array \
+        --disable-nls \
+        --disable-multilib \
+        --disable-decimal-float \
+        --disable-libatomic \
+        --disable-libgomp \
+        --disable-libquadmath \
+        --disable-libssp \
+        --disable-libvtv \
+        --disable-libstdcxx \
+        --enable-languages=c,c++
+    return
+}
+
+make_install() {
+    make && make -j1 DESTDIR=$LFS install && ln -sv gcc $LFS/usr/bin/cc
+    return
+}
+
+unpack_src && configure && make_install
