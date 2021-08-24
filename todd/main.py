8e1b43e0e21ba069628159cb9da16c2d3295c3d2
Adam Jędrzejewski <adamjedrzejewski@icloud.com>
2021-08-24 15:16:01 +0200

    main arg parser

--- todd/main.py
+++ todd/main.py
@@ -2,9 +2,18 @@
 import sys
 
 from check_req import check_reqs, check_sym_links
+from argparse import ArgumentParser
 
 
 def main() -> int:
+    parser = ArgumentParser(description="Run Todd Linux build system")
+    parser.add_argument('path', help='path to chroot environment', type=str)
+    parser.add_argument('-t','--time', help='measure build time', action='store_true')
+    parser.add_argument('-q','--quiet', help='don\'t print messages from underlaying processes', action='store_true')
+    parser.add_argument('-j','--jobs', help='number of concurrent jobs (if not specified `nproc` output is used)')
+    args = parser.parse_args()
+
+
     print("checking requirements: ...")
     all_ok = True
     if not check_reqs():
