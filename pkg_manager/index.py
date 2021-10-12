92ce7daba53ff3b97f1edb76e8584c14bacb809e
christopher-besch <christopher.besch@gmx.de>
2021-10-12 15:07:24 +0200

    formatter be weird

--- pkg_manager/index.py
+++ pkg_manager/index.py
@@ -22,21 +22,22 @@ def append_index(lfs_dir: str, new_pkg: PackageIndex) -> None:
     if new_pkg.name in pkgs:
         raise ValueError(f"Can't append already installed package {new_pkg}")
 
+
 def remove_index(lfs_dir: str, del_pkg: PackageIndex) -> None:
     pkgs = read_index(lfs_dir)
     if del_pkg.name not in pkgs:
         raise ValueError(f"Can't remove index of not installed package {del_pkg}")
+
 
 def read_index(lfs_dir: str) -> Dict[str, PackageIndex]:
     index: Dict[str, PackageIndex] = {}
     with open(f"{lfs_dir}/var/lib/todd/status.json", "r") as file:
         pkgs_json = json.load(file)
         for pkg_json in pkgs_json:
-            index[pkg_json["name"]] = PackageIndex(pkg_json["name"], pkg_json["version"],pkg_json["files"])
+            index[pkg_json["name"]] = PackageIndex(pkg_json["name"], pkg_json["version"], pkg_json["files"])
     return index
 
 
 def update_index(lfs_dir: str, index: Dict[str, PackageIndex]) -> None:
     with open(f"{lfs_dir}/var/lib/todd/status.json", "w") as file:
         json.dump(index, file)
-
